f0cd5487b450c902f60090ccaaa848d07914dac2
ArtemAbramov <ho4hyiiika@yandex.ru>
2024-01-23 18:19:07 +0300

    Добавьте файлы проекта.

---
+++ bl2_7.2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+#define SUB(a, b) ((a)-(b))
+
+
+int main(int argc, char** argv)
+{
+	int a = 8;
+	int b = 3;
+	int c = 2;
+
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+}
